245abcdc7d36faa05e46a0994850250325752bb5
Francisco Cortez <franciscocortezdev@gmail.com>
2022-07-12 17:32:03 -0600

    Contact section form

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.contactForm')
+const emailBtn = document.querySelector('.emailBtn')
+
+form.addEventListener('submit', (e) => {
+  
+  DataForm = new FormData(form)
+  
+  emailBtn.setAttribute('href', `mailto:franciscocortezdev@gmail.com?subject=${DataForm.get('Name')} - ${DataForm.get('Email')}&body=Subject: ${DataForm.get('Subject')} Message: ${DataForm.get('Message')}`)
+  
+  emailBtn.click()
+})
+
 
