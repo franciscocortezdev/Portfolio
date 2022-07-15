3ff9e94d38e54ca267b41f0617dcbd7c18ca485a
Francisco Cortez <franciscocortezdev@gmail.com>
2022-07-14 18:12:21 -0600

    Menu link current funtionality

--- code.js
+++ code.js
@@ -1,24 +1,57 @@
-const form = document.querySelector('.contactForm')
-const emailBtn = document.querySelector('.emailBtn')
-const labelDown = document.getElementById('btnScrollDown')
+const form = document.querySelector(".contactForm");
+const emailBtn = document.querySelector(".emailBtn");
+const labelDown = document.getElementById("btnScrollDown");
+const pageSections = document.querySelectorAll(".section");
+const linksMenu = document.querySelectorAll(".Nav-Link");
 
-form.addEventListener('submit', (e) => {
-  
-  DataForm = new FormData(form)
-  
-  emailBtn.setAttribute('href', `mailto:franciscocortezdev@gmail.com?subject=${DataForm.get('Name')} - ${DataForm.get('Email')}&body=Subject: ${DataForm.get('Subject')} Message: ${DataForm.get('Message')}`)
-  
-  emailBtn.click()
-})
+// Form Contact
+form.addEventListener("submit", (e) => {
+  DataForm = new FormData(form);
 
+  emailBtn.setAttribute(
+    "href",
+    `mailto:franciscocortezdev@gmail.com?subject=${DataForm.get(
+      "Name"
+    )} - ${DataForm.get("Email")}&body=Subject: ${DataForm.get(
+      "Subject"
+    )} Message: ${DataForm.get("Message")}`
+  );
 
-window.addEventListener('scroll',()=>{
-  
-  if(window.scrollY > 100){
-    labelDown.style.display = 'none'
+  emailBtn.click();
+});
+
+//Label ScrollDown
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    labelDown.style.display = "none";
   }
-  if(window.scrollY < 100){
-    labelDown.style.display = 'flex'
+  if (window.scrollY < 100) {
+    labelDown.style.display = "flex";
   }
+});
 
-})
+// Menu status
+
+let options = {
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+const handleMenu = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      linksMenu.forEach((link) => {
+        link.classList.remove("navLinkCurrent");
+
+        if (link.classList.contains(`${entry.target.id}Link`)) {
+          link.classList.add("navLinkCurrent");
+        }
+      });
+    }
+  });
+};
+
+let observer = new IntersectionObserver(handleMenu, options);
+let link = document.querySelector(".aboutLink");
+pageSections.forEach((el) => {
+  observer.observe(el);
+});
